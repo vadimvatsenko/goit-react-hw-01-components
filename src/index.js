a1059ca5567fd2a321c32bdfede379491e144ca3
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-11-09 12:31:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";// импортируем реакт
+import ReactDOM from "react-dom/client";// импорт DOM
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = React.createElement(
+  'div', {
+    a: 5, b: 10,
+  children: 'hello word',
+  });// создается едемент div c пропсами, children: 'hello word'
+console.log(element);
+
+ReactDom.createRoot(element, document.querySelector('#root'));// рендер елемнта в root
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
